21835a85303e3d47dfc232afcd44b286d95c4146
zxhuen <zxhuen324@gmail.com>
2026-08-07 12:23:20 +0800

    load chat endpoint and service done

--- app/api/chatbot.py
+++ app/api/chatbot.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.core.limiter import limiter
 from app.schemas.chat_schema import ChatCreate
+from app.services.chat_service.chat_services import initialize_chat_session
 from app.services.login_service.login import login_user
 from app.core.security import oauth2_scheme
 
@@ -15,7 +16,19 @@ def chat_luna(
     request: Request,
     chat: ChatCreate,
     response: Response,
-    session_token: str | None = Cookie(default=None)
+    session_token: str | None = Cookie(default=None),
     db: Session = Depends(get_db),
 ):
-    
+    print()
+
+
+@router.get("/Load-Chat")
+@limiter.limit("5/minute")
+def load_chat(
+    request: Request,
+    response: Response,
+    public_id: str,
+    session_token: str | None = Cookie(default=None),
+    db: Session = Depends(get_db),
+):
+    return initialize_chat_session(public_id, response, session_token, db)
